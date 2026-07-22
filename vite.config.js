35ee9f14da8b16d4a71e7b1b4675beb4fad6d677
Marvin Hoo <hoom@oregonstate.edu>
2026-07-22 16:38:22 -0400

    chore: fix vite base path

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/BudgetProj/',
+  base: '/budgetProj/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['pyodide'],
