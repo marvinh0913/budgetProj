b1d95d4d9806ff206ad7df00a77b60cf5f33389c
Marvin Hoo <hoom@oregonstate.edu>
2026-07-22 18:24:25 -0400

    feat: add logo to navbar and browser tab

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,11 +3,14 @@ import { QuestionMarkCircleIcon } from '@heroicons/react/24/outline';
 import '../styles/navbar.css';
 
 function NavBar() {
+  const logoUrl = import.meta.env.DEV ? '/logo.png' : '/budgetProj/logo.png';
+
   return (
     <nav className="navbar" role="navigation">
       <div className="navbar-container">
         <Link to="/" className="navbar-brand">
           BudgetProj
+          <img src={logoUrl} alt="BudgetProj logo" className="navbar-logo" />
         </Link>
         <div className="navbar-links">
           <Link to="/faq" className="navbar-faq-link">
